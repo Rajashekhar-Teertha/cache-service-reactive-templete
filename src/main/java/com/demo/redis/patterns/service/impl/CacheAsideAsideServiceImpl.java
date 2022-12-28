e2c26174c50d280345abf6375d7f658589d709f0
Rajashekhar Teertha <rajashekhar.teertha@maersk.com>
2022-12-28 13:02:31 +0530

    Update CacheAsideAsideServiceImpl.java

--- src/main/java/com/demo/redis/patterns/service/impl/CacheAsideAsideServiceImpl.java
+++ src/main/java/com/demo/redis/patterns/service/impl/CacheAsideAsideServiceImpl.java
@@ -21,28 +21,22 @@ public class CacheAsideAsideServiceImpl<K,V> implements CacheAsideService<K,V> {
     ReactiveRedisTemplate<Object,Object> reactiveRedisTemplate;
     @Override
     public Mono<V> getCache(K key) {
-        try{
             log.info("reading from cache with value key {} ",key);
-            return (Mono<V>) reactiveRedisTemplate.opsForValue().get(key);
-        }
-        catch(Exception e){
-            log.error("exception occurred while fetching data from cache {}", e.getMessage());
-            throw new CacheProcessingException(e.getMessage());
-        }
+            return (Mono<V>) reactiveRedisTemplate.opsForValue().get(key).doOnError(e -> {
+                log.error("exception occurred while fetching data from cache {}", e.getMessage());
+                throw new CacheProcessingException(e.getMessage());
+            });
     }
 
     @Override
     public Mono<Void> putCache(K id, V object) {
-        try{
             log.info("writing to cache with value key {} , object {}",id,object);
-            return reactiveRedisTemplate.opsForValue().set(id, object).then();
+            return reactiveRedisTemplate.opsForValue().set(id, object).then().doOnError(e -> {
+                log.error("exception occurred while processing the cache {}", e.getMessage());
+                throw new CacheProcessingException(e.getMessage());
+            });
 //            return reactiveRedisTemplateforBooking.opsForValue().set(id, object,
 //                    Duration.ofSeconds(Long.parseLong(config.getCacheTTL()))).then();
-        }
-        catch(Exception e){
-            log.error("exception occurred while processing the cache {}", e.getMessage());
-            throw new CacheProcessingException(e.getMessage());
-        }
     }
 
 }
